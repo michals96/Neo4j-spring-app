f05a5741f70478fe62dfb838151c04b28514a104
Michal Stefaniuk <michal.stefaniuk@motorolasolutions.com>
2020-11-12 20:19:13 +0100

    Search by release date

--- src/main/resources/static/styles/cssandjs/actions.js
+++ src/main/resources/static/styles/cssandjs/actions.js
@@ -42,3 +42,23 @@ function searchByTitle(moviesNames, moviesScores, moviesRelease){
     }
 }
 
+function searchByYear(moviesNames, moviesScores, moviesRelease){
+    var date = document.getElementById('search_year').value;
+    document.getElementById("results").innerHTML = "";
+    var table = document.getElementById("results");
+    var counter = 0;
+
+    for (i = 0; i < moviesNames.length; i++) {
+        if(moviesRelease[i] == date){
+            var row = table.insertRow(counter);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            cell1.innerHTML = moviesNames[i];
+            cell2.innerHTML = moviesRelease[i];
+            cell3.innerHTML = moviesScores[i];
+            counter++;
+        }
+    }
+}
+
