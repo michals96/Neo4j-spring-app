ce5594d90052290e9029297ea2632d066ef3038c
Michal Stefaniuk <michal.stefaniuk@motorolasolutions.com>
2020-11-12 22:57:31 +0100

    Clickable list

--- src/main/resources/static/styles/cssandjs/actions.js
+++ src/main/resources/static/styles/cssandjs/actions.js
@@ -14,16 +14,20 @@ for (i = 0; i < moviesList.length; i++) {
 function appendToTable(moviesNames, moviesScores, moviesRelease){
 
 var table = document.getElementById("tablebody");
-for (i = 0; i < moviesNames.length; i++) {
 
+for (i = 0; i < moviesNames.length; i++) {
         var row = table.insertRow(i);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
-        cell1.innerHTML = moviesNames[i];
+        cell1.innerHTML = '<h2 id='+i+' value ="mybutton" onclick="krzycze(this.id)">' + moviesNames[i] + '</h2>';
         cell2.innerHTML = moviesRelease[i];
         cell3.innerHTML = moviesScores[i];
     }
+}
+
+function krzycze(val){
+    alert(val);
 }
 
 function searchByTitle(moviesNames, moviesScores, moviesRelease){
