84ac3ea8518e7d2ad1e390be9974120049e64fc9
michals96 <michal.stefaniuk9@gmail.com>
2020-11-13 00:23:46 +0100

    Call controler service from HTML

--- src/main/java/com/example/demo/controllers/MovieLibraryController.java
+++ src/main/java/com/example/demo/controllers/MovieLibraryController.java
@@ -5,6 +5,11 @@ import org.neo4j.driver.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class MovieLibraryController {
@@ -29,6 +34,30 @@ public class MovieLibraryController {
             model.addAttribute("moviesScores",  session.run("MATCH (n:Anime) RETURN n LIMIT 25").list(r -> r.get("n").asNode().get("score").asInt()));
             model.addAttribute("moviesRelease", session.run("MATCH (n:Anime) RETURN n LIMIT 25").list(r -> r.get("n").asNode().get("released").asInt()));
         }
+        return "mainPage";
+    }
+
+    /*@RequestMapping("/test")
+    public String test(@RequestParam(value = "name", required = false) String name, Model model){
+        try(Session session = driver.session()){
+           System.out.println("*********************** " + name + " **********************");
+        }
+        return "mainPage";
+    }*/
+
+    @RequestMapping("/test")
+    public String index(
+            @RequestParam(value = "participant", required = false) String participant,
+            @RequestParam(value = "country", required = false) String country,
+            @RequestParam(value = "action", required = false) String action,
+            @RequestParam(value = "id", required = false) Integer id,
+            Model model
+    ) {
+        model.addAttribute("id", id);
+        List<Integer> userIds = Arrays.asList(1,2,3,4);
+        model.addAttribute("userIds", userIds);
+        model.addAttribute("par", participant);
+        System.out.println("KUPA");
         return "mainPage";
     }
 }
--- src/main/resources/static/styles/cssandjs/actions.js
+++ src/main/resources/static/styles/cssandjs/actions.js
@@ -20,7 +20,7 @@ for (i = 0; i < moviesNames.length; i++) {
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
-        cell1.innerHTML = '<h2 id='+i+' value ="mybutton" onclick="krzycze(this.id)">' + moviesNames[i] + '</h2>';
+        cell1.innerHTML = '<a th:href="@{/test(name="value1")}">' + moviesNames[i] + '</a>';
         cell2.innerHTML = moviesRelease[i];
         cell3.innerHTML = moviesScores[i];
     }
