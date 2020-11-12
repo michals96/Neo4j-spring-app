58653a90ff11006ccf070a9c282e31574549febd
Michal Stefaniuk <michal.stefaniuk@motorolasolutions.com>
2020-11-12 12:00:38 +0100

    Serving studios in html

--- src/main/java/com/example/demo/controllers/MovieLibraryController.java
+++ src/main/java/com/example/demo/controllers/MovieLibraryController.java
@@ -3,12 +3,15 @@ package com.example.demo.controllers;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class MovieLibraryController {
     private final Driver driver;
 
@@ -19,7 +22,21 @@ public class MovieLibraryController {
     @GetMapping(value = "/movieLibrary", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<String> getActors() throws Exception {
         try (Session session = driver.session()){
-            return session.run("MATCH (n:Actor) RETURN n LIMIT 25").list(r -> r.get("n").asNode().get("name").asString());
+            return session.run("MATCH (n:Anime) RETURN n LIMIT 25").list(r -> r.get("n").asNode().get("title").asString());
         }
+    }
+
+    @GetMapping(value = "/getStudios")
+    public String getStudios(Model model) throws Exception {
+        try (Session session = driver.session()){
+            model.addAttribute("studiosList",session.run("MATCH (n:Studio) RETURN n LIMIT 25").list(r -> r.get("n").asNode().get("name").asString()));
+        }
+        return "getStudios";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
