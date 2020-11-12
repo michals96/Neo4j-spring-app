cd94aa8349e068236232a08f58796f0e866dcd4f
michals96 <michal.stefaniuk9@gmail.com>
2020-11-13 00:55:11 +0100

    Fixed calling controller from HTML

--- src/main/java/com/example/demo/controllers/MovieLibraryController.java
+++ src/main/java/com/example/demo/controllers/MovieLibraryController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
@@ -27,36 +28,13 @@ public class MovieLibraryController {
         return "testpage";
     }
 
-    @GetMapping("/")
-    public String getAllMovies(Model model){
+    @RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST } )
+    public String getAllMovies(@RequestParam(value = "participant", required = false) String participant, Model model){
         try(Session session = driver.session()){
             model.addAttribute("moviesNames",   session.run("MATCH (n:Anime) RETURN n LIMIT 25").list(r -> r.get("n").asNode().get("title").asString()));
             model.addAttribute("moviesScores",  session.run("MATCH (n:Anime) RETURN n LIMIT 25").list(r -> r.get("n").asNode().get("score").asInt()));
             model.addAttribute("moviesRelease", session.run("MATCH (n:Anime) RETURN n LIMIT 25").list(r -> r.get("n").asNode().get("released").asInt()));
         }
-        return "mainPage";
-    }
-
-    /*@RequestMapping("/test")
-    public String test(@RequestParam(value = "name", required = false) String name, Model model){
-        try(Session session = driver.session()){
-           System.out.println("*********************** " + name + " **********************");
-        }
-        return "mainPage";
-    }*/
-
-    @RequestMapping("/test")
-    public String index(
-            @RequestParam(value = "participant", required = false) String participant,
-            @RequestParam(value = "country", required = false) String country,
-            @RequestParam(value = "action", required = false) String action,
-            @RequestParam(value = "id", required = false) Integer id,
-            Model model
-    ) {
-        model.addAttribute("id", id);
-        List<Integer> userIds = Arrays.asList(1,2,3,4);
-        model.addAttribute("userIds", userIds);
-        model.addAttribute("par", participant);
         System.out.println("KUPA");
         return "mainPage";
     }
